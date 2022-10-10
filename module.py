ae8440835138cd55c786e254f29754d662c4a6ac
fgaillar <florian.gaillard@student.unamur.be>
2022-10-10 21:15:23 +0200

    version2

--- module.py
+++ module.py
@@ -591,7 +591,9 @@ def get_creature_life(creature):
 
     return game_db['creatures'][creature]['life']
 
-def reach(variety):
+def restart_game():
+    reset_game()
+def reach_character(variety):
     """
     calcul the reach of the variety of the character.
 
@@ -604,22 +606,33 @@ def reach(variety):
     ValueError: if the character does not exist
 
     """
-    reach = ""
     if variety == "elf" or variety == "wizard":
-        reach = 1
+        reach = 'short'
     else:
-        reach = 0
+        reach = 'long'
+    return reach
+def character_life(variety):
+    if variety == "dwarf":
+        life = random.randint(10,50)
+    elif variety == "healer" or 'wizard' or 'necromancer':
+        life = random.randint(5,15)
+    elif variety == "elf":
+        life = random.randint(15,25)
+    return life
+def character_strength(variety):
+        if variety == "dwarf":
+            strength = random.randint(10, 50)
+        elif variety == "healer" or 'wizard' or 'necromancer':
+            strength = random.randint(5, 15)
+        elif variety == "elf":
+            strength = random.randint(15, 25)
+        return strength
 
-
-def start():
-    _load_game_db()
-def restart_game():
-    reset_game()
-    start()
 def create_new_player(character,variety):
-    reach = ""
-    life = 0
-    strenght = 0
-    reach(variety)
-    set_character_life(character,)
-    return character, 'is a', variety, 'and have', life, 'health point', 'and', strenght, 'of power'
+    character_exists(character)
+    reach = reach_character(variety)
+    life = character_life(variety)
+    strength = character_strength(variety)
+    return character,variety,reach,life,strength
+
+create_new_player("Greudon",'necromancer')
