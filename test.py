d74c59a8c5fc95dafbc193b2ffb49f0262c04a64
fgaillar <florian.gaillard@student.unamur.be>
2022-10-12 10:30:52 +0200

    2.6

--- test.py
+++ test.py
@@ -7,13 +7,19 @@ def fight(character, creature):
                 gt.set_creature_life(creature, (gt.get_creature_life(creature) - gt.get_character_strength(character)))
             if gt.get_character_strength(character) >= gt.get_creature_life(creature):
                 gt.set_creature_life(creature, 0)
+                print('The creature %s is now dead by taking %d damage from %s' % (creature, gt.get_character_strength(character), character))
         else:
             print('This character is dead')
     else:
         print('You don\'t have the good reach')
 
-    if not gt.get_character_reach(character) == 'short' and gt.get_creature_reach(creature) == 'long':
-
+    if not gt.get_creature_reach(creature) == 'short' and gt.get_character_reach(character) == 'long':
+        if gt.get_character_life(character) > 0:
+            if gt.get_creature_strength(creature) < gt.get_character_life(character):
+                gt.set_character_life(character, (gt.get_character_life(character) - gt.get_creature_strength(creature)))
+                print('The character %s has been hurt by %d from %s' % (character, gt.get_creature_strength(creature), creature))
+            if gt.get_character_strength(creature) >= gt.get_character_life(character):
+                gt.set_character_life(character, 0)
 
 def attack(character, creature):
     if gt.get_character_reach(character) == gt.get_creature_reach(creature):
